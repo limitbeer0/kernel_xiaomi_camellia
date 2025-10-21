d27e31a51d85dd6501afa9ba022ef58486a609eb
ayushqui <proayush736@gmail.com>
2025-10-21 14:21:05 +0530

    sound: soc: Import Xiaomi changes

--- sound/soc/mediatek/mt6833/mt6833-misc-control.c
+++ sound/soc/mediatek/mt6833/mt6833-misc-control.c
@@ -14,12 +14,18 @@
 
 #include "../common/mtk-afe-fe-dai.h"
 #include "../common/mtk-afe-platform-driver.h"
+#if defined(CONFIG_MTK_VOW_BARGE_IN_SUPPORT)
 #include "../scp_vow/mtk-scp-vow-common.h"
+#endif
 
 #include "mt6833-afe-common.h"
 
 #define SGEN_MUTE_CH1_KCONTROL_NAME "Audio_SineGen_Mute_Ch1"
 #define SGEN_MUTE_CH2_KCONTROL_NAME "Audio_SineGen_Mute_Ch2"
+
+/* BSP.Audio - 2020.12.06 - modify to add kcontrol for closing AGC */
+extern int fsm_add_control(struct snd_soc_platform *platform);
+/* end modify */
 
 static const char * const mt6833_sgen_mode_str[] = {
 	"I0I1",   "I2",     "I3I4",   "I5I6",
@@ -2016,25 +2022,24 @@ static const struct snd_kcontrol_new mt6833_afe_speech_controls[] = {
 		       speech_property_get, speech_property_set),
 };
 
+#if defined(CONFIG_MTK_VOW_BARGE_IN_SUPPORT)
 /* VOW barge in control */
 static int mt6833_afe_vow_bargein_get(struct snd_kcontrol *kcontrol,
 				      struct snd_ctl_elem_value *ucontrol)
 {
-#if defined(CONFIG_MTK_VOW_SUPPORT)
 	struct snd_soc_component *cmpnt = snd_soc_kcontrol_component(kcontrol);
 	struct mtk_base_afe *afe = snd_soc_component_get_drvdata(cmpnt);
 	int id;
 
 	id = get_scp_vow_memif_id();
 	ucontrol->value.integer.value[0] = afe->memif[id].vow_bargein_enable;
-#endif
+
 	return 0;
 }
 
 static int mt6833_afe_vow_bargein_set(struct snd_kcontrol *kcontrol,
 				      struct snd_ctl_elem_value *ucontrol)
 {
-#if defined(CONFIG_MTK_VOW_SUPPORT)
 	struct snd_soc_component *cmpnt = snd_soc_kcontrol_component(kcontrol);
 	struct mtk_base_afe *afe = snd_soc_component_get_drvdata(cmpnt);
 	int id;
@@ -2045,7 +2050,7 @@ static int mt6833_afe_vow_bargein_set(struct snd_kcontrol *kcontrol,
 	dev_info(afe->dev, "%s(), %d\n", __func__, val);
 
 	afe->memif[id].vow_bargein_enable = (val > 0) ? true : false;
-#endif
+
 	return 0;
 }
 
@@ -2054,6 +2059,7 @@ static const struct snd_kcontrol_new mt6833_afe_bargein_controls[] = {
 		       mt6833_afe_vow_bargein_get,
 		       mt6833_afe_vow_bargein_set),
 };
+#endif
 
 int mt6833_add_misc_control(struct snd_soc_platform *platform)
 {
@@ -2075,9 +2081,15 @@ int mt6833_add_misc_control(struct snd_soc_platform *platform)
 				      mt6833_afe_speech_controls,
 				      ARRAY_SIZE(mt6833_afe_speech_controls));
 
+/* BSP.Audio - 2020.12.06 - modify to add kcontrol for closing AGC */
+	fsm_add_control(platform);
+/* end modify */
+
+#if defined(CONFIG_MTK_VOW_BARGE_IN_SUPPORT)
 	snd_soc_add_platform_controls(platform,
 				      mt6833_afe_bargein_controls,
 				      ARRAY_SIZE(mt6833_afe_bargein_controls));
+#endif
 
 	return 0;
 }
